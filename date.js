fef31f89485a0c775ab295d5d198448e4e84d76c
Felipe Miiller <felipemiillerr@gmail.com>
2023-08-31 15:20:00 -0300

    Update date.js

--- date.js
+++ date.js
@@ -1,1 +1,22 @@
+const dateNow = (Date.now());
+
+function formatDate(date) {
+    const handlerDate = new Date(date);
+    const locale = 'pt-BR';
+    return handlerDate.toLocaleString(locale, {
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+    });
+}
+
+console.log(formatDate(dateNow))
+
+
+new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'full',
+}).format(date)
+
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Intl
 
