3a5f3a2b8faee6388545aba588e6ed668ed10a98
Felipe Miiller <felipemiillerr@gmail.com>
2024-06-15 08:21:18 -0300

    Update date.js

--- date.js
+++ date.js
@@ -17,6 +17,21 @@ new Intl.DateTimeFormat('pt-BR', {
     dateStyle: 'full',
 }).format(date)
 
+export function formateDate(date?: string, separator?: boolean): string {
+  const type: boolean =
+    separator == true || separator == undefined ? true : false
+  const dateObj = date ? new Date(date) : new Date()
+  const dia = date
+    ? String(dateObj.getDate() + 1).padStart(2, "0")
+    : String(dateObj.getDate()).padStart(2, "0")
+  const mes = String(dateObj.getMonth() + 1).padStart(2, "0")
+  const ano = String(dateObj.getFullYear()).padStart(4, "0")
+
+  if (type) {
+    return `${dia}/${mes}/${ano}`
+  }
+  return dia + mes + ano
+}
 
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Intl
 
